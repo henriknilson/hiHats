d4bc37c82ac30e257b31618284fe96ea1f582956
Fredrik Kindstrom <fredrik.kindstrom@gmail.com>
2015-09-17 21:13:08 +0200

    made action bar Green and fixed crash

--- app/src/main/java/hihats/electricity/MainActivity.java
+++ app/src/main/java/hihats/electricity/MainActivity.java
@@ -1,5 +1,6 @@
 package hihats.electricity;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -7,7 +8,7 @@ import android.view.MenuItem;
 import com.parse.Parse;
 import com.parse.ParseObject;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
