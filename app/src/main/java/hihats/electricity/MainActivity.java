c9f959a8dcd3bf7ad8172642c6e7b921c2c71354
Henrik Nilson <henrik.nilson91@gmail.com>
2015-09-18 18:05:52 +0200

    modified mainactivity to launch lginandsignup

--- app/src/main/java/hihats/electricity/MainActivity.java
+++ app/src/main/java/hihats/electricity/MainActivity.java
@@ -1,37 +1,34 @@
 package hihats.electricity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.parse.*;
+public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        // Check if user is logged in or not
+        if (ParseAnonymousUtils.isLinked(ParseUser.getCurrentUser())) {
+            // If user is not logged in send to Log in class
+            Intent intent = new Intent(MainActivity.this, LogInAndSignUpActivity.class);
+            startActivity(intent);
+            finish();
+        } else {
+            // If user is logged in, get username
+            ParseUser currentUser = ParseUser.getCurrentUser();
+            if (currentUser != null) {
+                Intent intent = new Intent(MainActivity.this, Home.class);
+                startActivity(intent);
+                finish();
+            } else {
+                Intent intent = new Intent(MainActivity.this,
+                        LogInAndSignUpActivity.class);
+                startActivity(intent);
+                finish();
+            }
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
