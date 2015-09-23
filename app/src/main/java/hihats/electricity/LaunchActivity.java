17b8a6627a862316fed19b5d803789e3717c45ff
Fredrik Kindstrom <fredrik.kindstrom@gmail.com>
2015-09-23 14:46:31 +0200

    renamed MainActivity to LaunchActivity and deleted TabActivity

--- app/src/main/java/hihats/electricity/LaunchActivity.java
+++ app/src/main/java/hihats/electricity/LaunchActivity.java
@@ -4,7 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import com.parse.*;
-public class MainActivity extends Activity {
+public class LaunchActivity extends Activity {
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -12,18 +12,18 @@ public class MainActivity extends Activity {
         // Check if user is logged in or not
         if (ParseAnonymousUtils.isLinked(ParseUser.getCurrentUser())) {
             // If user is not logged in send to Log in class
-            Intent intent = new Intent(MainActivity.this, LogInAndSignUpActivity.class);
+            Intent intent = new Intent(LaunchActivity.this, LogInAndSignUpActivity.class);
             startActivity(intent);
             finish();
         } else {
             // If user is logged in, get username
             ParseUser currentUser = ParseUser.getCurrentUser();
             if (currentUser != null) {
-                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                Intent intent = new Intent(LaunchActivity.this, HomeActivity.class);
                 startActivity(intent);
                 finish();
             } else {
-                Intent intent = new Intent(MainActivity.this,
+                Intent intent = new Intent(LaunchActivity.this,
                         LogInAndSignUpActivity.class);
                 startActivity(intent);
                 finish();
