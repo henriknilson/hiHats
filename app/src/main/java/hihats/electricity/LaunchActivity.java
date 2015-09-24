09dad1f40a0fc42b20eff5feee66066ab1f0cbe9
Henrik Nilson <henrik.nilson91@gmail.com>
2015-09-24 14:24:28 +0200

    removed parse application added it to lanuch instead

--- app/src/main/java/hihats/electricity/LaunchActivity.java
+++ app/src/main/java/hihats/electricity/LaunchActivity.java
@@ -9,6 +9,17 @@ public class LaunchActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        Parse.enableLocalDatastore(this);
+        Parse.initialize(this, "w5w8u7YeyDApblFiC9XPn509REPNdFIv1SleClrR", "IxqGzyFnJUENwSpNl4vRajZaW9gPPgPdK3cArFse");
+
+        ParseUser.enableAutomaticUser();
+        ParseACL defaultACL = new ParseACL();
+
+        // If you would like all objects to be private by default, remove this
+        // line.
+        defaultACL.setPublicReadAccess(true);
+        ParseACL.setDefaultACL(defaultACL, true);
+
         // Check if user is logged in or not
         if (ParseAnonymousUtils.isLinked(ParseUser.getCurrentUser())) {
             // If user is not logged in send to Log in class
