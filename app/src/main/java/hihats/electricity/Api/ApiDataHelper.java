4b1360a45f5a2d5dc19a1b563ab2c53d336e4457
Fredrik Kindstrom <fredrik.kindstrom@gmail.com>
2015-09-24 13:30:09 +0200

    added getDistance method and added more javadoc

--- app/src/main/java/hihats/electricity/Api/ApiDataHelper.java
+++ app/src/main/java/hihats/electricity/Api/ApiDataHelper.java
@@ -1,55 +1,64 @@
 package hihats.electricity.api;
 
 import android.location.Location;
-import java.util.Date;
 
 /**
- * Created by fredrikkindstrom on 24/09/15.
+ * This class converts the data obtained by the backend class
+ * ApiHttpHandler to useful formats to use further up in the application.
+ * This is the class that gets called from outside the api package.
  */
 public class ApiDataHelper {
 
     /**
      * Returns the last known location for a certain bus.
-     * @param busId The bus you want to get current location for.
+     * @param busId The bus you want to get data for.
      * @return The most recent location for said bus as an Android Location object.
+     * @throws AccessErrorException When the http request failed and the data can not be obtained.
      */
     public static Location getMostRecentLocationForBus(String busId) throws AccessErrorException {
         ApiDataObject rawData = ApiHttpHandler.getMostRecentLocationForBus(busId);
         String data = rawData.getValue();
-        //Location location = new Location(busId);
-        double latitude;
-        double longitude;
-        System.out.println(data);
+        Location loc = new Location(busId);
+
         if (data.startsWith("$GPRMC")) {
             String[] strValues = data.split(",");
 
             // Set latitude
-            latitude = Double.parseDouble(strValues[3].substring(2))/60.0;
+            double latitude = Double.parseDouble(strValues[3].substring(2))/60.0;
             latitude +=  Double.parseDouble(strValues[3].substring(0, 2));
             if (strValues[4].charAt(0) == 'S') {
                 latitude = -latitude;
             }
-            //location.setLatitude(latitude);
+            loc.setLatitude(latitude);
 
             // Set longitude
-            longitude = Double.parseDouble(strValues[5].substring(3))/60.0;
+            double longitude = Double.parseDouble(strValues[5].substring(3))/60.0;
             longitude +=  Double.parseDouble(strValues[5].substring(0, 3));
             if (strValues[6].charAt(0) == 'W') {
                 longitude = -longitude;
             }
-            //location.setLongitude(longitude);
+            loc.setLongitude(longitude);
 
             // Set speed
             float speed = (Float.parseFloat(strValues[7])*1.85200f);
-            //location.setSpeed(speed);
+            loc.setSpeed(speed);
 
             // Set bearing
             float bearing = Float.parseFloat(strValues[8]);
-            //location.setBearing(bearing);
-
-            System.out.println(new Date(Long.parseLong(rawData.getTimestamp())));
-            System.out.println("latitude="+latitude+" ; longitude="+longitude+" ; speed = "+speed+" ; bearing = "+bearing);
+            loc.setBearing(bearing);
         }
-        return null;
+        return loc;
+    }
+
+    /**
+     * Returns the total distance traveled for a certain bus.
+     * @param busId The bus you want to get data for.
+     * @return The total distance in meters.
+     * @throws AccessErrorException When the http request failed and the data can not be obtained.
+     */
+    public static int getTotalDistanceForBus(String busId) throws AccessErrorException {
+        ApiDataObject rawData = ApiHttpHandler.getTotalDistanceForBus(busId);
+        int data = Integer.parseInt(rawData.getValue());
+        return data*5;
     }
 }
