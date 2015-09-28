bf3c1f8b225b910d5047618da4dae09ef9fd4d4f
Filip Hallqvist <filiphallqvist@gmail.com>
2015-09-28 23:26:26 +0200

    Styled tabs and almost fixed the RideFragment

--- app/src/main/java/hihats/electricity/activity/MainActivity.java
+++ app/src/main/java/hihats/electricity/activity/MainActivity.java
@@ -1,25 +1,16 @@
 package hihats.electricity.activity;
 
-import com.google.android.gms.maps.CameraUpdate;
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polyline;
-import com.google.android.gms.maps.model.PolylineOptions;
 import com.parse.ParseUser;
 
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -36,28 +27,6 @@ public class MainActivity extends AppCompatActivity {
 
     TabLayout tabLayout;
     Button logout;
-    Button changeMapView;
-
-    BusStop svenHultin;
-    BusStop chalmersPlatsen;
-    BusStop kapellPlatsen;
-    BusStop gotaPlatsen;
-    BusStop valand;
-    BusStop kungsPortsPlatsen;
-    BusStop brunnsParken;
-    BusStop lillaBommen;
-    BusStop friHamnen;
-    BusStop pumpGatan;
-    BusStop regnBagsGatan;
-    BusStop lindHolmen;
-    BusStop lindHolmsPlatsen;
-    BusStop teknikGatan;
-
-    //Map Variables
-    boolean mShowMap;
-    GoogleMap mMap;
-    Polyline line;
-    ArrayList<BusStop> busStops;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -70,140 +39,30 @@ public class MainActivity extends AppCompatActivity {
                 MainActivity.this));
 
         // Give the TabLayout the ViewPager
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
         tabLayout.setupWithViewPager(viewPager);
-    }
 
-//        setupToolbar();
-//        setupTablayout();
-//        mShowMap = initMap();
-//        setupMap();
-//        drawPath();
-//
-//        // Retrieve current user from Parse.com
-//        ParseUser currentUser = ParseUser.getCurrentUser();
-//
-//        // Convert currentUser into String
-//        String struser = currentUser.getUsername().toString();
-//
-//        // Locate TextView in activity_main.xml
-//        TextView txtuser = (TextView) findViewById(R.id.txtuser);
-//
-//        // Set the currentUser String into TextView
-//        txtuser.setText("You are logged in as " + struser);
-//
-//        // Locate Button in activity_main.xml
-//        logout = (Button) findViewById(R.id.logout);
-//        changeMapView = (Button) findViewById(R.id.changeMapView);
-//
-//        // Store shit
-//
-//        // Logout Button Click Listener
-//        logout.setOnClickListener(new OnClickListener() {
-//
-//            public void onClick(View arg0) {
-//                // Logout current user
-//                ParseUser.logOut();
-//                Intent intent = new Intent(MainActivity.this, LogInAndSignUpActivity.class);
-//                startActivity(intent);
-//                finish();
-//            }
-//        });
-//
-//        changeMapView.setOnClickListener(new OnClickListener() {
-//
-//            public void onClick(View arg0) {
-//            // Change map style
-//                if(mMap.getMapType() == GoogleMap.MAP_TYPE_NORMAL){
-//                    mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
-//                }else
-//                    mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-//            }
-//        });
-//    }
-//
-//    private boolean initMap() {
-//        if(mMap == null){
-//            MapFragment mapFrag = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
-//            mMap = mapFrag.getMap();
-//        }
-//        return mMap != null;
-//    }
-//
-//    private void setupMap() {
-//        //temp. latlng, later to be replaced with cellphone latlng
-//        LatLng latlng = new LatLng(57.68857167,11.97830168);
-//
-//        //Busstop variables
-//        svenHultin = new BusStop(57.685825, 11.977261, "Sven Hultins Gata");
-//        chalmersPlatsen = new BusStop(57.689312, 11.973452, "Chalmersplatsen");
-//        kapellPlatsen = new BusStop(57.693730, 11.973338, "Kapellplatsen");
-//        gotaPlatsen = new BusStop(57.697652, 11.978949, "Götaplatsen");
-//        valand = new BusStop(57.700347, 11.974572, "Valand");
-//        kungsPortsPlatsen = new BusStop(57.704038, 11.969529, "Kungsportsplatsen");
-//        brunnsParken = new BusStop(57.706962, 11.967620, "Brunnsparken");
-//        lillaBommen = new BusStop(57.709549, 11.965952, "Lilla Bommen");
-//        friHamnen = new BusStop(57.718204, 11.959474, "Frihamnsporten");
-//        pumpGatan = new BusStop(57.712793, 11.946173, "Pumpgatan");
-//        regnBagsGatan = new BusStop(57.710765, 11.942761, "Regnbågsgatan");
-//        lindHolmen = new BusStop(57.708105, 11.938089, "Lindholmen");
-//        teknikGatan = new BusStop(57.706907, 11.937150, "Teknikgatan");
-//        lindHolmsPlatsen = new BusStop(57.706993, 11.938464, "Lindholmsplatsen");
-//
-//        //Array containing the busstops
-//        busStops = new ArrayList<>();
-//        busStops.add(svenHultin);
-//        busStops.add(chalmersPlatsen);
-//        busStops.add(kapellPlatsen);
-//        busStops.add(gotaPlatsen);
-//        busStops.add(valand);
-//        busStops.add(kungsPortsPlatsen);
-//        busStops.add(brunnsParken);
-//        busStops.add(lillaBommen);
-//        busStops.add(friHamnen);
-//        busStops.add(pumpGatan);
-//        busStops.add(regnBagsGatan);
-//        busStops.add(lindHolmen);
-//        busStops.add(teknikGatan);
-//        busStops.add(lindHolmsPlatsen);
-//
-//        //Direct camera to given position and add markers
-//        if(mShowMap){
-//            CameraUpdate update = CameraUpdateFactory.newLatLngZoom(latlng,15);
-//            mMap.moveCamera(update);
-//            mMap.addMarker(new MarkerOptions()
-//                    .position(latlng)
-//                    .title("You are here!")
-//            );
-//            mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-//            for(int i = 0; i < busStops.size(); i++){
-//                mMap.addMarker(new MarkerOptions()
-//                        .position(busStops.get(i).getLatLng())
-//                        .title(busStops.get(i).getName())
-//                );
-//            }
-//        }
-//    }
-//
-//    private void setupToolbar(){
-//        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
-//    }
-//
-//    private void setupTablayout(){
-//        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
-//        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
-//        tabLayout.addTab(tabLayout.newTab().setText(getResources().getString(R.string.title_section1)));
-//        tabLayout.addTab(tabLayout.newTab().setText(getResources().getString(R.string.title_section2)));
-//        tabLayout.addTab(tabLayout.newTab().setText(getResources().getString(R.string.title_section3)));
-//    }
-//
-//    private void drawPath(){
-//        PolylineOptions options = new PolylineOptions().width(5).color(Color.BLACK).geodesic(true);
-//        for(int i=0; i < busStops.size(); i++){
-//            LatLng point = busStops.get(i).getLatLng();
-//            options.add(point);
-//        }
-//        line = mMap.addPolyline(options);
-//    }
+        // Retrieve current user from Parse.com
+        ParseUser currentUser = ParseUser.getCurrentUser();
+
+        // Convert currentUser into String
+        String struser = currentUser.getUsername().toString();
+
+        // Locate Button in activity_main.xml
+        logout = (Button) findViewById(R.id.logout);
+
+        // Logout Button Click Listener
+        logout.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View arg0) {
+                // Logout current user
+                ParseUser.logOut();
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+    }
 }
--- app/src/main/java/hihats/electricity/fragment/RideFragment.java
+++ app/src/main/java/hihats/electricity/fragment/RideFragment.java
@@ -1,5 +1,6 @@
 package hihats.electricity.fragment;
 
+import android.graphics.Color;
 import android.support.v4.app.Fragment;
 import android.net.Uri;
 import android.os.Bundle;
@@ -7,19 +8,56 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.GoogleApiClient.*;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import java.util.ArrayList;
 
 import hihats.electricity.R;
+import hihats.electricity.model.BusStop;
 
 public class RideFragment extends Fragment implements ConnectionCallbacks, OnConnectionFailedListener {
 
     private OnFragmentInteractionListener mListener;
     private GoogleApiClient mGoogleApiClient;
+    SupportMapFragment mMapFragment;
     private static String TAG = "RideFragment";
+
+    Button changeMapView;
+
+    BusStop svenHultin;
+    BusStop chalmersPlatsen;
+    BusStop kapellPlatsen;
+    BusStop gotaPlatsen;
+    BusStop valand;
+    BusStop kungsPortsPlatsen;
+    BusStop brunnsParken;
+    BusStop lillaBommen;
+    BusStop friHamnen;
+    BusStop pumpGatan;
+    BusStop regnBagsGatan;
+    BusStop lindHolmen;
+    BusStop lindHolmsPlatsen;
+    BusStop teknikGatan;
+
+    //Map Variables
+    boolean mShowMap;
+    GoogleMap mMap;
+    Polyline line;
+    ArrayList<BusStop> busStops;
+
 
     public static RideFragment newInstance() {
         RideFragment fragment = new RideFragment();
@@ -33,8 +71,27 @@ public class RideFragment extends Fragment implements ConnectionCallbacks, OnCon
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_ride, container, false);
+                             Bundle savedInstanceState) {;
+
+        View view = inflater.inflate(R.layout.fragment_ride, container, false);
+
+
+        changeMapView = (Button) view.findViewById(R.id.changeMapView);
+
+        mMapFragment = new SupportMapFragment() {
+            @Override
+            public void onActivityCreated(Bundle savedInstanceState) {
+                super.onActivityCreated(savedInstanceState);
+                mMap = mMapFragment.getMap();
+                if (mMap != null) {
+                    setupMap();
+                    drawPath();
+                }
+            }
+        };
+        getChildFragmentManager().beginTransaction().add(R.id.rideFragment, mMapFragment).commit();
+
+        return view;
     }
 
     public void onButtonPressed(Uri uri) {
@@ -68,6 +125,78 @@ public class RideFragment extends Fragment implements ConnectionCallbacks, OnCon
 
     public interface OnFragmentInteractionListener {
         public void onFragmentInteraction(Uri uri);
+    }
+
+    private boolean initMap() {
+        if(mMap == null){
+            SupportMapFragment mapFrag = (SupportMapFragment) getFragmentManager().findFragmentById(R.id.map);
+            mMap = mapFrag.getMap();
+        }
+        return mMap != null;
+    }
+
+    private void setupMap() {
+        //temp. latlng, later to be replaced with cellphone latlng
+        LatLng latlng = new LatLng(57.68857167,11.97830168);
+
+        //Busstop variables
+        svenHultin = new BusStop(57.685825, 11.977261, "Sven Hultins Gata");
+        chalmersPlatsen = new BusStop(57.689312, 11.973452, "Chalmersplatsen");
+        kapellPlatsen = new BusStop(57.693730, 11.973338, "Kapellplatsen");
+        gotaPlatsen = new BusStop(57.697652, 11.978949, "Götaplatsen");
+        valand = new BusStop(57.700347, 11.974572, "Valand");
+        kungsPortsPlatsen = new BusStop(57.704038, 11.969529, "Kungsportsplatsen");
+        brunnsParken = new BusStop(57.706962, 11.967620, "Brunnsparken");
+        lillaBommen = new BusStop(57.709549, 11.965952, "Lilla Bommen");
+        friHamnen = new BusStop(57.718204, 11.959474, "Frihamnsporten");
+        pumpGatan = new BusStop(57.712793, 11.946173, "Pumpgatan");
+        regnBagsGatan = new BusStop(57.710765, 11.942761, "Regnbågsgatan");
+        lindHolmen = new BusStop(57.708105, 11.938089, "Lindholmen");
+        teknikGatan = new BusStop(57.706907, 11.937150, "Teknikgatan");
+        lindHolmsPlatsen = new BusStop(57.706993, 11.938464, "Lindholmsplatsen");
+
+        //Array containing the busstops
+        busStops = new ArrayList<>();
+        busStops.add(svenHultin);
+        busStops.add(chalmersPlatsen);
+        busStops.add(kapellPlatsen);
+        busStops.add(gotaPlatsen);
+        busStops.add(valand);
+        busStops.add(kungsPortsPlatsen);
+        busStops.add(brunnsParken);
+        busStops.add(lillaBommen);
+        busStops.add(friHamnen);
+        busStops.add(pumpGatan);
+        busStops.add(regnBagsGatan);
+        busStops.add(lindHolmen);
+        busStops.add(teknikGatan);
+        busStops.add(lindHolmsPlatsen);
+
+        //Direct camera to given position and add markers
+        if(mShowMap){
+            CameraUpdate update = CameraUpdateFactory.newLatLngZoom(latlng, 15);
+            mMap.moveCamera(update);
+            mMap.addMarker(new MarkerOptions()
+                    .position(latlng)
+                    .title("You are here!")
+            );
+            mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+            for(int i = 0; i < busStops.size(); i++){
+                mMap.addMarker(new MarkerOptions()
+                        .position(busStops.get(i).getLatLng())
+                        .title(busStops.get(i).getName())
+                );
+            }
+        }
+    }
+
+    private void drawPath(){
+        PolylineOptions options = new PolylineOptions().width(5).color(Color.BLACK).geodesic(true);
+        for(int i=0; i < busStops.size(); i++){
+            LatLng point = busStops.get(i).getLatLng();
+            options.add(point);
+        }
+        line = mMap.addPolyline(options);
     }
 
 }
