3e4f6ef89298e03a86a4a9c900888a75a4e590ff
Filip Hallqvist <filiphallqvist@gmail.com>
2015-09-28 21:45:52 +0200

    Deleted old BusrideFragment

--- app/src/main/java/hihats/electricity/fragment/RideFragment.java
+++ app/src/main/java/hihats/electricity/fragment/RideFragment.java
@@ -3,15 +3,23 @@ package hihats.electricity.fragment;
 import android.support.v4.app.Fragment;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.*;
+import com.google.android.gms.location.LocationServices;
+
 import hihats.electricity.R;
 
-public class RideFragment extends Fragment {
+public class RideFragment extends Fragment implements ConnectionCallbacks, OnConnectionFailedListener {
 
     private OnFragmentInteractionListener mListener;
+    private GoogleApiClient mGoogleApiClient;
+    private static String TAG = "RideFragment";
 
     public static RideFragment newInstance() {
         RideFragment fragment = new RideFragment();
@@ -33,6 +41,29 @@ public class RideFragment extends Fragment {
         if (mListener != null) {
             mListener.onFragmentInteraction(uri);
         }
+    }
+
+    protected synchronized void buildGoogleApiClient() {
+        Log.i(TAG, "Building GoogleApiClient");
+        mGoogleApiClient = new GoogleApiClient.Builder(this.getActivity())
+                .addConnectionCallbacks(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
     }
 
     public interface OnFragmentInteractionListener {
