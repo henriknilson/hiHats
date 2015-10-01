4130816fde952de1514f8c7b64c457bf7fa4128d
Fredrik Kindstrom <fredrik.kindstrom@gmail.com>
2015-10-02 00:26:23 +0200

    CHANGED: Ride class structure

--- app/src/main/java/hihats/electricity/model/Ride.java
+++ app/src/main/java/hihats/electricity/model/Ride.java
@@ -7,26 +7,18 @@ import java.util.Date;
  */
 public class Ride {
 
+    private Bus bus;
     private int distance;
-    private int points;
-    private Date date;
 
-    public Ride(int distance, int points, Date date) {
-        this.distance = distance;
-        this.points = points;
-        this.date = date;
+    public Ride(Bus bus) {
+        this.bus = bus;
+    }
+
+    public void startRide() {
+        
     }
 
     public void appendDistance(int distance) {
         this.distance += distance;
-    }
-
-    public void updatePoints() {
-        // TODO fancy points algorithm
-        points = distance * 12;
-    }
-
-    public void setDate(Date date) {
-        this.date = date;
     }
 }
