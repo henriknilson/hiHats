ee93e07f07623d7fadafe5cd95077c6b8f9f5ab4
Fredrik Kindstrom <fredrik.kindstrom@gmail.com>
2015-10-04 16:37:46 +0200

    created XML parse method

--- app/src/main/java/hihats/electricity/util/FindBusHelper.java
+++ app/src/main/java/hihats/electricity/util/FindBusHelper.java
@@ -4,9 +4,23 @@ import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+import java.io.IOException;
+import java.io.StringReader;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
 import hihats.electricity.net.AccessErrorException;
 import hihats.electricity.net.HttpHandler;
-import hihats.electricity.net.NoDataException;
 
 /**
  * Created by fredrikkindstrom on 03/10/15.
@@ -23,8 +37,41 @@ public class FindBusHelper {
     }
 
     public String askNetworkForId() throws AccessErrorException {
-        String response;
-        response = httpHandler.getResponse("http://www.gettingagile.com/feed/rss2/");
-        return response;
+        String response = httpHandler.getResponse("http://feeds.feedburner.com/entrepreneur/startingabusiness.rss");
+        String result = parseFromXML(response);
+        if (result != null) {
+            return result;
+        } else {
+            throw new AccessErrorException();
+        }
+    }
+
+    /*
+    Help methods
+     */
+
+    private String parseFromXML(String xml) {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder;
+        Document document = null;
+        final String GROUP = "channel";
+        final String VALUE = "title";
+        String result = null;
+        try {
+            builder = factory.newDocumentBuilder();
+            document = builder.parse(new InputSource(new StringReader(xml)));
+        } catch (ParserConfigurationException | SAXException | IOException e) {
+            e.printStackTrace();
+        }
+        document.getDocumentElement().normalize();
+        NodeList nList = document.getElementsByTagName(GROUP);
+        for (int temp = 0; temp < nList.getLength(); temp++) {
+            Node nNode = nList.item(temp);
+            if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+                Element eElement = (Element) nNode;
+                result = eElement.getElementsByTagName(VALUE).item(0).getTextContent();
+            }
+        }
+        return result;
     }
 }
