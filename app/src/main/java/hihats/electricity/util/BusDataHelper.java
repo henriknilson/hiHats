5a1ef67296d023d630d2469604af5f741858444a
Fredrik Kindstrom <fredrik.kindstrom@gmail.com>
2015-10-06 16:58:18 +0200

    refactored BusDataHelper with new Bus object

--- app/src/main/java/hihats/electricity/util/BusDataHelper.java
+++ app/src/main/java/hihats/electricity/util/BusDataHelper.java
@@ -32,76 +32,22 @@ public class BusDataHelper {
      */
 
     /**
-     * Returns the last known location for a certain bus.
-     * @param busId The bus you want to get data for.
-     * @return The most recent location for said bus as a Location object.
+     * Updates the bus object with the last known complete data for the bus.
+     * @param bus The bus you want to update data for.
      * @throws AccessErrorException When the http request failed and the data can not be obtained.
      * @throws NoDataException When the http request was successful but no data was found.
      */
-    public Location getLastLocationForBus(String busId) throws AccessErrorException, NoDataException {
-        String url = urlRetriever.getUrl(busId, null, GPS_RMC, 5000);
+    public void updateDataForBus(Bus bus) throws AccessErrorException, NoDataException {
+        String url = urlRetriever.getUrl(bus.getDgw(), null, GPS_RMC, 5000);
         String response = httpHandler.getResponse(url);
         ArrayList<ApiDataObject> rawData = parseFromJSON(response);
         ApiDataObject data = rawData.get(0);
-        return RmcConverter.rmcToLocation(data.getValue(), data.getTimestamp());
-    }
-
-    /**
-     * Returns if the bus is currently located at a bus stop or not.
-     * @param busId The bus you want to get data for.
-     * @return True if the bus is at a bus stop, false if not.
-     * @throws AccessErrorException When the http request failed and the data can not be obtained.
-     * @throws NoDataException When the http request was successful but no data was found.
-     */
-    public boolean isBusAtStop(String busId) throws AccessErrorException, NoDataException {
-        String url = urlRetriever.getUrl(busId, null, AT_STOP, 30000);
-        String response = httpHandler.getResponse(url);
-        ArrayList<ApiDataObject> rawData = parseFromJSON(response);
-        System.out.println(rawData.size());
-        ApiDataObject data = rawData.get(0);
-        switch (data.getValue()) {
-            case "true":
-                return true;
-            case "false":
-                return false;
-            default:
-                throw new NoDataException();
-        }
-    }
-
-    /**
-     * Returns the name of the next bus stop for a bus.
-     * @param busId The bus you want to get data for.
-     * @return A String with the name of the bus stop.
-     * @throws AccessErrorException When the http request failed and the data can not be obtained.
-     * @throws NoDataException When the http request was successful but no data was found.
-     */
-    public String getNextStopForBus(String busId) throws AccessErrorException, NoDataException {
-        String url = urlRetriever.getUrl(busId, null, NEXT_STOP, 15000);
-        System.out.println(url);
-        String response = httpHandler.getResponse(url);
-        ArrayList<ApiDataObject> rawData = parseFromJSON(response);
-        ApiDataObject data = rawData.get(0);
-        return data.getValue();
-    }
-
-    /**
-     * Returns the last known complete data for a certain bus.
-     * @param busId The bus you want to get data for.
-     * @return A Bus object containing all the data obtained.
-     * @throws AccessErrorException When the http request failed and the data can not be obtained.
-     * @throws NoDataException When the http request was successful but no data was found.
-     */
-    public Bus getLastDataForBus(String busId) throws AccessErrorException, NoDataException {
-        String url = urlRetriever.getUrl(busId, null, GPS_RMC, 5000);
-        String response = httpHandler.getResponse(url);
-        ArrayList<ApiDataObject> rawData = parseFromJSON(response);
-        ApiDataObject data = rawData.get(0);
-        String id = data.getGatewayId();
         Location loc = RmcConverter.rmcToLocation(data.getValue(), data.getTimestamp());
         float speed = RmcConverter.rmcToSpeed(data.getValue());
         float bearing = RmcConverter.rmcToBearing(data.getValue());
-        return new Bus(id, loc, speed, bearing);
+        bus.setLocation(loc);
+        bus.setSpeed(speed);
+        bus.setBearing(bearing);
     }
 
     /**
@@ -118,26 +64,80 @@ public class BusDataHelper {
         for (ApiDataObject o : rawData) {
             String id = o.getGatewayId();
             Location loc = RmcConverter.rmcToLocation(o.getValue(), o.getTimestamp());
-            // float speed = RmcConverter.rmcToSpeed(o.getValue());
-            // float bearing = RmcConverter.rmcToBearing(o.getValue());
-            buses.add(new Bus(id, loc));
+            Bus bus = new Bus(id);
+            bus.setLocation(loc);
+            buses.add(bus);
         }
         return buses;
     }
 
     /**
+     * Returns the last known location for a certain bus.
+     * @param bus The bus you want to get data for.
+     * @return The most recent location for said bus as a Location object.
+     * @throws AccessErrorException When the http request failed and the data can not be obtained.
+     * @throws NoDataException When the http request was successful but no data was found.
+     */
+    public Location getLastLocationForBus(Bus bus) throws AccessErrorException, NoDataException {
+        String url = urlRetriever.getUrl(bus.getDgw(), null, GPS_RMC, 5000);
+        String response = httpHandler.getResponse(url);
+        ArrayList<ApiDataObject> rawData = parseFromJSON(response);
+        ApiDataObject data = rawData.get(0);
+        return RmcConverter.rmcToLocation(data.getValue(), data.getTimestamp());
+    }
+
+    /**
+     * Returns the name of the next bus stop for a bus.
+     * @param bus The bus you want to get data for.
+     * @return A String with the name of the bus stop.
+     * @throws AccessErrorException When the http request failed and the data can not be obtained.
+     * @throws NoDataException When the http request was successful but no data was found.
+     */
+    public String getNextStopForBus(Bus bus) throws AccessErrorException, NoDataException {
+        String url = urlRetriever.getUrl(bus.getDgw(), null, NEXT_STOP, 15000);
+        System.out.println(url);
+        String response = httpHandler.getResponse(url);
+        ArrayList<ApiDataObject> rawData = parseFromJSON(response);
+        ApiDataObject data = rawData.get(0);
+        return data.getValue();
+    }
+
+    /**
      * Returns the total distance traveled for a certain bus.
-     * @param busId The bus you want to get data for.
+     * @param bus The bus you want to get data for.
      * @return The total distance in meters.
      * @throws AccessErrorException When the http request failed and the data can not be obtained.
      * @throws NoDataException When the http request was successful but no data was found.
      */
-    public int getTotalDistanceForBus(String busId) throws AccessErrorException, NoDataException {
-        String url = urlRetriever.getUrl(busId, null, TOTAL_VEHICLE_DISTANCE, 10000);
+    public int getTotalDistanceForBus(Bus bus) throws AccessErrorException, NoDataException {
+        String url = urlRetriever.getUrl(bus.getDgw(), null, TOTAL_VEHICLE_DISTANCE, 10000);
         String response = httpHandler.getResponse(url);
         ArrayList<ApiDataObject> rawData = parseFromJSON(response);
         int data = Integer.parseInt(rawData.get(0).getValue());
         return data * 5;
+    }
+
+    /**
+     * Returns if the bus is currently located at a bus stop or not.
+     * @param bus The bus you want to get data for.
+     * @return True if the bus is at a bus stop, false if not.
+     * @throws AccessErrorException When the http request failed and the data can not be obtained.
+     * @throws NoDataException When the http request was successful but no data was found.
+     */
+    public boolean isBusAtStop(Bus bus) throws AccessErrorException, NoDataException {
+        String url = urlRetriever.getUrl(bus.getDgw(), null, AT_STOP, 30000);
+        String response = httpHandler.getResponse(url);
+        ArrayList<ApiDataObject> rawData = parseFromJSON(response);
+        System.out.println(rawData.size());
+        ApiDataObject data = rawData.get(0);
+        switch (data.getValue()) {
+            case "true":
+                return true;
+            case "false":
+                return false;
+            default:
+                throw new NoDataException();
+        }
     }
 
     /*
