d75273d7b91622e0d6c693d7600988b54bc891bb
Fredrik Kindstrom <fredrik.kindstrom@gmail.com>
2015-10-06 19:40:44 +0200

    added Google API Client

--- app/src/main/java/hihats/electricity/activity/MainActivity.java
+++ app/src/main/java/hihats/electricity/activity/MainActivity.java
@@ -1,7 +1,9 @@
 package hihats.electricity.activity;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 import com.parse.ParseUser;
-
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -14,20 +16,27 @@ import android.widget.Button;
 import hihats.electricity.R;
 import hihats.electricity.adapter.TabsAdapter;
 
-
 /**
  * Created by henriknilson on 18/09/15.
  */
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     Toolbar toolbar;
     TabLayout tabLayout;
     Button logout;
+    public GoogleApiClient googleApiClient;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Create a GoogleApiClient instance
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
 
         // Get the ViewPager and set it's PagerAdapter so that it can display items
         ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
@@ -58,5 +67,41 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        googleApiClient.connect();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (googleApiClient.isConnected()) {
+            googleApiClient.disconnect();
+        }
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        // Connected to Google Play services!
+        // The good stuff goes here.
+        System.out.println("GOOGLE API CONNECTED");
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        // The connection has been interrupted.
+        // Disable any UI components that depend on Google APIs
+        // until onConnected() is called.
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        // This callback is important for handling errors that
+        // may occur while attempting to connect with Google.
+        //
+        // More about this in the 'Handle Connection Failures' section.
     }
 }
