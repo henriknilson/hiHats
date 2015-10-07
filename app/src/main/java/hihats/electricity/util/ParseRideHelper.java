5f8dcdf3c8ca6bcf64e8de6296333ed4f4952b7e
Aringskog <a.aringskog@gmail.com>
2015-10-07 14:25:14 +0200

    added parseridehelper

---
+++ app/src/main/java/hihats/electricity/util/ParseRideHelper.java
@@ -0,0 +1,33 @@
+package hihats.electricity.util;
+
+import com.parse.ParseClassName;
+import com.parse.ParseObject;
+
+import java.util.Date;
+
+import hihats.electricity.model.BusStop;
+
+/**
+ * Created by axel on 2015-10-07.
+ */
+@ParseClassName("ParseRideHelper")
+public class ParseRideHelper extends ParseObject {
+
+    public ParseRideHelper(){}
+
+    public Date getDate(){
+        return getDate("date");
+    }
+
+    public String getBusStopFrom(){
+        return getString("busStopFrom");
+    }
+
+    public String getBusStopToo(){
+        return getString("busStopToo");
+    }
+
+    public int getPoints(){
+        return getInt("points");
+    }
+}
