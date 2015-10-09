1ce9a05ecabf3b0fe8a0816de354538f5e6c550b
Fredrik Kindstrom <fredrik.kindstrom@gmail.com>
2015-10-09 11:05:06 +0200

    gradle conflict

--- app/src/main/java/hihats/electricity/util/BusDataHelper.java
+++ app/src/main/java/hihats/electricity/util/BusDataHelper.java
@@ -1,9 +1,28 @@
 package hihats.electricity.util;
 
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
 import com.google.gson.Gson;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+import java.io.IOException;
+import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.Collections;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
 import hihats.electricity.model.Bus;
 import hihats.electricity.model.SimpleLocation;
@@ -28,26 +47,76 @@ public class BusDataHelper {
     private final HttpHandler httpHandler = new HttpHandler();
 
     /*
-    Util methods
+    Boolean methods
      */
 
     /**
-     * Updates the bus object with the last known complete data for the bus.
-     * @param bus The bus you want to update data for.
+     * Returns if the bus is currently located at a bus stop or not.
+     * @param bus The bus you want to get data for.
+     * @return True if the bus is at a bus stop, false if not.
      * @throws AccessErrorException When the http request failed and the data can not be obtained.
      * @throws NoDataException When the http request was successful but no data was found.
      */
-    public void updateDataForBus(Bus bus) throws AccessErrorException, NoDataException {
-        String url = urlRetriever.getUrl(bus.getDgw(), null, GPS_RMC, 5000);
+    public boolean isBusAtStop(Bus bus) throws AccessErrorException, NoDataException {
+        String url = urlRetriever.getUrl(bus.getDgw(), null, AT_STOP, 30000);
         String response = httpHandler.getResponse(url);
         ArrayList<ApiDataObject> rawData = parseFromJSON(response);
+        System.out.println(rawData.size());
         ApiDataObject data = rawData.get(0);
-        SimpleLocation loc = RmcConverter.rmcToLocation(data.getValue(), data.getTimestamp());
-        float speed = RmcConverter.rmcToSpeed(data.getValue());
-        float bearing = RmcConverter.rmcToBearing(data.getValue());
-        bus.setSimpleLocation(loc);
-        bus.setSpeed(speed);
-        bus.setBearing(bearing);
+        switch (data.getValue()) {
+            case "true":
+                return true;
+            case "false":
+                return false;
+            default:
+                throw new NoDataException();
+        }
+    }
+
+    public boolean isConnectedToWifi(Context context) {
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo wifiNetwork = cm.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        return wifiNetwork != null && wifiNetwork.isConnected();
+    }
+
+    public boolean isGPSEnabled(Context context) {
+        LocationManager locManager = (LocationManager)context.getSystemService(Context.LOCATION_SERVICE);
+        return locManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+    }
+
+    /*
+    Get data methods
+     */
+
+    public Bus getBusFromSystemId() throws AccessErrorException, NoDataException {
+        String response = httpHandler.getResponse("http://feeds.feedburner.com/entrepreneur/startingabusiness.rss");
+        String result = parseFromXML(response);
+        if (result != null) {
+            try {
+                return new Bus(result);
+            } catch (IllegalArgumentException e) {
+                throw new NoDataException();
+            }
+        } else {
+            throw new AccessErrorException();
+        }
+    }
+
+    public Bus getBusNearestLocation(Location location) throws AccessErrorException, NoDataException {
+        ArrayList<Bus> allBuses = getLastDataForAllBuses();
+        for (Bus bus : allBuses) {
+            float[] distanceBetweenBuses = new float[1];
+            Location.distanceBetween(
+                    bus.getSimpleLocation().getLatitude(),
+                    bus.getSimpleLocation().getLongitude(),
+                    location.getLatitude(),
+                    location.getLongitude(),
+                    distanceBetweenBuses);
+            if (distanceBetweenBuses[0] < 20) {
+                return bus;
+            }
+        }
+        return null;
     }
 
     /**
@@ -117,32 +186,61 @@ public class BusDataHelper {
         return data * 5;
     }
 
+    /*
+    Void update methods
+     */
+
     /**
-     * Returns if the bus is currently located at a bus stop or not.
-     * @param bus The bus you want to get data for.
-     * @return True if the bus is at a bus stop, false if not.
+     * Updates the bus object with the last known complete data for the bus.
+     * @param bus The bus you want to update data for.
      * @throws AccessErrorException When the http request failed and the data can not be obtained.
      * @throws NoDataException When the http request was successful but no data was found.
      */
-    public boolean isBusAtStop(Bus bus) throws AccessErrorException, NoDataException {
-        String url = urlRetriever.getUrl(bus.getDgw(), null, AT_STOP, 30000);
+    public void updateDataForBus(Bus bus) throws AccessErrorException, NoDataException {
+        String url = urlRetriever.getUrl(bus.getDgw(), null, GPS_RMC, 5000);
         String response = httpHandler.getResponse(url);
         ArrayList<ApiDataObject> rawData = parseFromJSON(response);
-        System.out.println(rawData.size());
         ApiDataObject data = rawData.get(0);
-        switch (data.getValue()) {
-            case "true":
-                return true;
-            case "false":
-                return false;
-            default:
-                throw new NoDataException();
-        }
+        SimpleLocation loc = RmcConverter.rmcToLocation(data.getValue(), data.getTimestamp());
+        float speed = RmcConverter.rmcToSpeed(data.getValue());
+        float bearing = RmcConverter.rmcToBearing(data.getValue());
+        bus.setSimpleLocation(loc);
+        bus.setSpeed(speed);
+        bus.setBearing(bearing);
     }
 
     /*
     Help methods
      */
+
+    private String parseFromXML(String xml) {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder;
+        Document document = null;
+        final String GROUP = "channel";
+        final String VALUE = "title";
+        String result = null;
+        try {
+            builder = factory.newDocumentBuilder();
+            document = builder.parse(new InputSource(new StringReader(xml)));
+        } catch (ParserConfigurationException | SAXException | IOException e) {
+            e.printStackTrace();
+        }
+        if (document != null) {
+            document.getDocumentElement().normalize();
+            NodeList nList = document.getElementsByTagName(GROUP);
+            for (int temp = 0; temp < nList.getLength(); temp++) {
+                Node nNode = nList.item(temp);
+                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+                    Element eElement = (Element) nNode;
+                    result = eElement.getElementsByTagName(VALUE).item(0).getTextContent();
+                }
+            }
+            return result;
+        } else {
+            return null;
+        }
+    }
 
     private ArrayList<ApiDataObject> parseFromJSON(String s) throws NoDataException {
         Gson gson = new Gson();
