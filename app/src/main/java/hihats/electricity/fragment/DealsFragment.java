a95c886d336e57fb6d5a6e17c760d7946d8699ac
Filip Hallqvist <filiphallqvist@gmail.com>
2015-10-20 21:35:57 +0200

    Optimized images and added some more parse data to deals

--- app/src/main/java/hihats/electricity/fragment/DealsFragment.java
+++ app/src/main/java/hihats/electricity/fragment/DealsFragment.java
@@ -34,6 +34,12 @@ public class DealsFragment extends Fragment {
      */
     List<HashMap<String, String>> deals;
 
+    int[] dealImages = new int[] {
+            R.drawable.forest,
+            R.drawable.mountain,
+            R.drawable.bananas
+    };
+
     public static DealsFragment newInstance() {
         DealsFragment fragment = new DealsFragment();
         return fragment;
@@ -60,7 +66,8 @@ public class DealsFragment extends Fragment {
                 "name",
                 "author",
                 "description",
-                "points"
+                "points",
+                "image"
         };
 
         // "... and put their values in these places..."
@@ -68,7 +75,8 @@ public class DealsFragment extends Fragment {
                 R.id.dealName,
                 R.id.dealAuthor,
                 R.id.dealDescription,
-                R.id.dealPoints
+                R.id.dealPoints,
+                R.id.dealImage
         };
 
         // ...and use these arrays arrays to build a ListView adapter.
@@ -105,7 +113,11 @@ public class DealsFragment extends Fragment {
                         deal.put("description", parseObject.getString("description"));
                         deal.put("points", Integer.toString(
                                         parseObject.getNumber("points").intValue()
-                                )
+                                ) + " GreenPoints"
+                        );
+                        deal.put("image", Integer.toString(
+                                dealImages[Integer.parseInt(parseObject.getString("image"))]
+                            )
                         );
 
                         deals.add(deal);
