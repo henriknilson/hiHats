c71f4bcd581c3ee10b5ebe4b25034090b3924ade
Fredrik Kindstrom <fredrik.kindstrom@gmail.com>
2015-10-28 12:58:47 +0100

    more cleanup

--- app/src/main/java/hihats/electricity/net/NoDataException.java
+++ app/src/main/java/hihats/electricity/net/NoDataException.java
@@ -3,14 +3,13 @@ package hihats.electricity.net;
 /**
  * Created by fredrikkindstrom on 24/09/15.
  */
-public class NoDataException extends Exception
-{
-    //Parameterless Constructor
+public class NoDataException extends Exception {
+
+    // No parameter Constructor
     public NoDataException() {}
 
-    //Constructor that accepts a message
-    public NoDataException(String message)
-    {
+    // Constructor that accepts a message
+    public NoDataException(String message) {
         super(message);
     }
 }
