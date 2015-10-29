2b7c1ca611b91c65eed8cdbfa13f8f78d53ce74a
Fredrik Kindstrom <fredrik.kindstrom@gmail.com>
2015-10-29 16:54:07 +0100

    added DBEvent

---
+++ app/src/main/java/hihats/electricity/database/DBEvent.java
@@ -0,0 +1,21 @@
+package hihats.electricity.database;
+
+/**
+ * Created by fredrikkindstrom on 29/10/15.
+ */
+public enum DBEvent {
+
+    /*
+    Download events
+     */
+
+    BUSSTOPS_DOWNLOADED,
+    RIDES_DOWNLOADED,
+
+    /*
+    Upload events
+     */
+
+    RIDE_UPLOADED,
+
+}
