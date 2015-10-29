d52b3af97737cc66e5031b3d9343b8eee437bdca
Filip Hallqvist <filiphallqvist@gmail.com>
2015-10-29 20:52:32 +0100

    Added comments to IBusStop

--- app/src/main/java/hihats/electricity/model/IBusStop.java
+++ app/src/main/java/hihats/electricity/model/IBusStop.java
@@ -7,7 +7,18 @@ import com.google.android.gms.maps.model.LatLng;
  */
 public interface IBusStop extends Comparable<IBusStop> {
 
+    /**
+     * @return The coordinates of the bus stop.
+     */
     LatLng getLatLng();
+
+    /**
+     * @return The name of the bus stop.
+     */
     String getName();
+
+    /**
+     * @return The order of the bus stop, compared to all the other bus stops.
+     */
     int getOrder();
 }
